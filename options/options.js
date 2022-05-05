b2d5b018a970e4f6b4c6da671a103c6110de1ffe
Crowdbotics <team@crowdbotics.com>
2022-05-05 19:58:54 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "eurorite_exchange_35096",
+  url: "https://eurorite_exchange_35096.botics.co",
+  api: "https://eurorite_exchange_35096.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
